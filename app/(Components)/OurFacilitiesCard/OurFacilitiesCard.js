8a533d34e53b902435843b1cbfc19dbf6c788e57
Muzammilpro <muzammilhusnain7@gmail.com>
2023-08-23 01:26:07 +0500

    DO SOme Changes

--- app/(Components)/OurFacilitiesCard/OurFacilitiesCard.js
+++ app/(Components)/OurFacilitiesCard/OurFacilitiesCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function OurFacilitiesCard(props) {
   return (
     <>
     <div className="facilities-col">
-                <img src={props.scr} alt=""/>
-                <h3>{props.heading}</h3>
+                <Image src={props.scr} alt=""/>
+                <h3>{props.tittle}</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio omnis asperiores atque aperiam.
                 </p>
             </div>
