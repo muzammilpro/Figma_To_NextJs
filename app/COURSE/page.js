f57aead3e27ccb96e199d51dad02ef4e67bf52ad
Muzammilpro <muzammilhusnain7@gmail.com>
2023-08-23 11:50:31 +0500

    DO Some Changes

---
+++ app/COURSE/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../(Components)/Navbar/Navbar'
+import Footer from '../(Components)/Footer/Footer'
+
+export default function page() {
+  return (
+    <>
+      <section class="Sub-header">
+              <Navbar/>
+        <h1>Our Courses</h1>
+    </section>
+
+
+    <Footer/>
+    </>
+  )
+}
